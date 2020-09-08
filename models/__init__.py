016342f0c65f5b9db183a61125f762559471174b
Joseph D. Janizek <37272789+jjanizek@users.noreply.github.com>
2020-09-08 17:02:02 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 # __init__.py
-from models.cxrclassifierappa import CXRClassifierAPPA
+
 from models.cxrclassifier import CXRClassifier
 from models.cxrviewclassifier import CXRViewClassifier
-from models.pivotcxrclassifier import PivotCXRClassifier
-from models.irmclassifier import IRMClassifier
-from models.dann import DANN
