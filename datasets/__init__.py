56c3bc7b3b79e80e8ac174c410f534c1425be47f
Joseph D. Janizek <37272789+jjanizek@users.noreply.github.com>
2020-09-08 17:00:38 -0400

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,21 +3,10 @@
 from datasets.chestxray14dataset import ChestXray14Dataset
 from datasets.chestxray14h5 import ChestXray14H5Dataset
 
-from datasets.mimicdataset import MIMICDataset
-from datasets.mimich5 import MIMICH5Dataset
-
-from datasets.chexpertdataset import CheXpertDataset
-from datasets.chexperth5 import CheXpertH5Dataset
-
 from datasets.padchestdataset import PadChestDataset
 from datasets.padchesth5 import PadChestH5Dataset
 
-from datasets.combineddataset import CombinedDataset, CombinedDatasetResampled, CombinedDatasetMask, CombinedDatasetMaskMIMICCheXpert
-
-from datasets.auxdatasets import CheXpertDatasetAPPA, CheXpertDatasetMF, CheXpertDatasetGAN
-from datasets.auxdatasets import PadChestDatasetAPPA, PadChestDatasetMF
-from datasets.auxdatasets import ChestXray14DatasetAPPA, ChestXray14DatasetMF
-
 from datasets.githubcovid import GitHubCOVIDDataset
 from datasets.bimcvcovid import BIMCVCOVIDDataset
+
 from datasets.domainconfoundeddatasets import DomainConfoundedDataset
